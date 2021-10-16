7b6df2eaa3fedfda427afca9faedb4fc2e685879
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:29:48 +0700

    Tugas 15 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas15.js
@@ -0,0 +1,12 @@
+function panggilAku() {
+  var ab = 8;
+  var bc = 6;
+  var ac
+
+  console.log("Nilai AB = " + ab);
+  console.log("Nilai BC = " + bc);
+  ac = Math.pow(ab,2) + Math.pow(bc,2)
+  console.log("panjang sisi AC pada segitiga siku siku tersebut adalah : " + Math.sqrt(ac) + " cm");
+}
+
+panggilAku()
